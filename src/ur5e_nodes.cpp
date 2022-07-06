0eb39c71328c309138ca13257febcdc0ac908281
tammerb <thbarkouki@ucdavis.edu>
2022-07-06 20:16:37 +0000

    Use cartesian trajectory in MoveToPose node

--- src/ur5e_nodes.cpp
+++ src/ur5e_nodes.cpp
@@ -128,7 +128,23 @@ BT::NodeStatus MoveToPose::tick()
     static const std::string PLANNING_GROUP = "manipulator";
     moveit::planning_interface::MoveGroupInterface move_group_interface(PLANNING_GROUP);
 
-    move_group_interface.setMaxVelocityScalingFactor(1.0);
+    std::vector<geometry_msgs::Pose> waypoints;
+
+    geometry_msgs::Pose current_pose = move_group_interface.getCurrentPose().pose;
+    waypoints.push_back(current_pose);
+    geometry_msgs::Pose target_pose1 = current_pose;
+    target_pose1.position.z -= 0.2;
+    waypoints.push_back(target_pose1);
+
+    moveit_msgs::RobotTrajectory trajectory;
+    const double jump_threshold = 0.0;
+    const double eef_step = 0.01;
+    double fraction = move_group_interface.computeCartesianPath(waypoints, eef_step, jump_threshold, trajectory);
+
+    move_group_interface.execute(trajectory);
+
+    
+/*  move_group_interface.setMaxVelocityScalingFactor(1.0);
     move_group_interface.setMaxAccelerationScalingFactor(1.0);
     ROS_INFO("Planning frame: %s", move_group_interface.getPlanningFrame().c_str());
     ROS_INFO("End effector link: %s", move_group_interface.getEndEffectorLink().c_str());
@@ -136,12 +152,43 @@ BT::NodeStatus MoveToPose::tick()
     geometry_msgs::PoseStamped current_pose = move_group_interface.getCurrentPose();
     geometry_msgs::Pose target_pose = current_pose.pose;
 
-    target_pose.position.z = 0.1;
+    ROS_INFO("Current Pose x, y, z: %f, %f, %f", current_pose.pose.position.x, current_pose.pose.position.y, current_pose.pose.position.z);
+    target_pose.position.z = 0.298791;
 
     move_group_interface.setPoseTarget(target_pose);
+*/
+    bool debug = true;
+    if (debug || move_group_interface.execute(trajectory) == 1){
+        return BT::NodeStatus::SUCCESS;
+    } else {
+        return BT::NodeStatus::FAILURE;
+    }
+}
+
+BT::NodeStatus Pick::tick()
+{
+    std::cout << "Pick: " << this->name() << std::endl;
+    // setup the MoveGroupInterface
+    static const std::string PLANNING_GROUP = "manipulator";
+    moveit::planning_interface::MoveGroupInterface move_group_interface(PLANNING_GROUP);
+
+    std::vector<geometry_msgs::Pose> waypoints;
+
+    geometry_msgs::Pose current_pose = move_group_interface.getCurrentPose().pose;
+    waypoints.push_back(current_pose);
+    geometry_msgs::Pose target_pose1 = current_pose;
+    target_pose1.position.z -= 0.2;
+    waypoints.push_back(target_pose1);
+
+    moveit_msgs::RobotTrajectory trajectory;
+    const double jump_threshold = 0.0;
+    const double eef_step = 0.01;
+    double fraction = move_group_interface.computeCartesianPath(waypoints, eef_step, jump_threshold, trajectory);
+
+    move_group_interface.execute(trajectory);
 
     bool debug = true;
-    if (move_group_interface.move() == 1 || debug){
+    if (debug || move_group_interface.execute(trajectory) == 1){
         return BT::NodeStatus::SUCCESS;
     } else {
         return BT::NodeStatus::FAILURE;

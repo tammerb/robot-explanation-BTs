4b6ca083811203309da1e48ebdc088cba12da1ef
Ian Chuang <iantc104@gmail.com>
2023-10-06 22:59:47 -0700

    added logic to handle invert, retry, repeat for get next node

--- explain_bt/src/utils/utils.cpp
+++ explain_bt/src/utils/utils.cpp
@@ -85,7 +85,7 @@ namespace XBT
                 }
                 else
                 {
-                    // Succeed: Move to the next child of its parent.
+                    // Succeed: Return the next child of its parent.
                     return children[currentIndex + 1];
                 }
             }
@@ -101,8 +101,32 @@ namespace XBT
         }
         else if (auto decorator = dynamic_cast<const BT::DecoratorNode *>(p))
         {
-            // Propagate upwards since the decorator only has one child, which is the current node
-            return get_next_node_on_success(p);
+            if (dynamic_cast<const BT::InverterNode *>(p)) {
+                // propagate a failure upwards since succeed on inverter
+                return get_next_node_on_fail(p);
+            }
+            else if (auto repeat_node = dynamic_cast<const BT::RepeatNode *>(p)) {
+                if (repeat_node->do_loop()) {
+                    // repeat node is still looping on success, so return the current node
+                    return n;
+                }
+                else {
+                    // repeat node is done looping on success, so propagate upwards
+                    return get_next_node_on_success(p);
+                }
+            }
+            else if (dynamic_cast<const BT::RetryNode *>(p)) {
+                // propagate upwards since succeed on retry
+                return get_next_node_on_success(p);
+            }
+            else if (dynamic_cast<const BT::SubTreeNode *>(p)) {
+                // propagate upwards since succeed on subtree
+                return get_next_node_on_success(p);
+            }
+            else {
+                // Algorithm won't work with decorator nodes other than inverter, repeat, retry, and subtree, throw error 
+                throw std::runtime_error("XBT::get_next_node_on_success, Unexpected decorator node type, only inverter, repeat, retry, and subtree nodes are supported");
+            }
         }
         else 
         {
@@ -144,19 +168,43 @@ namespace XBT
                 }
                 else
                 {
-                    // fail on fallback, next node is right sibling
+                    // Fail: Return the next child of its parent.
                     return children[currentIndex + 1];
                 }
             }
             else {
                 // Algorithm won't work with control nodes other than sequence and fallback, throw error 
-                throw std::runtime_error("XBT::get_next_node_on_success, Unexpected control node type, only sequence and fallback nodes are supported");
+                throw std::runtime_error("XBT::get_next_node_on_fail, Unexpected control node type, only sequence and fallback nodes are supported");
             }
         }
         else if (auto decorator = dynamic_cast<const BT::DecoratorNode *>(p))
         {
-            // Propagate upwards since the decorator only has one child, which is the current node
-            return get_next_node_on_fail(p);
+            if (dynamic_cast<const BT::InverterNode *>(p)) {
+                // propagate a succeed upwards since failure on inverter
+                return get_next_node_on_success(p);
+            }
+            else if (dynamic_cast<const BT::RepeatNode *>(p)) {
+                // propagate upwards since failure on repeat
+                return get_next_node_on_fail(p);
+            }
+            else if (auto retry_node = dynamic_cast<const BT::RetryNode *>(p)) {
+                if (retry_node->do_loop()) {
+                    // retry node is still looping on failure, so return the current node
+                    return n;
+                }
+                else {
+                    // retry node is done looping on failure, so propagate upwards
+                    return get_next_node_on_fail(p);
+                }
+            }
+            else if (dynamic_cast<const BT::SubTreeNode *>(p)) {
+                // propagate upwards since failure on subtree
+                return get_next_node_on_fail(p);
+            }
+            else {
+                // Algorithm won't work with decorator nodes other than inverter, repeat, retry, and subtree, throw error 
+                throw std::runtime_error("XBT::get_next_node_on_fail, Unexpected decorator node type, only inverter, repeat, retry, and subtree nodes are supported");
+            }
         }
         else 
         {
